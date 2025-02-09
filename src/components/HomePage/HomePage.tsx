436e5457a16d52f6ddf158fc6c1caa7da5066ef6
damien <ancelin.damien@gmail.com>
2025-02-09 01:02:50 +0100

    Intégration > Avancé sur feature/homepage > Header - Footer > TODO: Mise en place des Product

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,3 +1,22 @@
+import Product from '../Product/Product';
+import Footer from './Footer/Footer';
+import Header from './Header/Header';
+
 export default function HomePage() {
-  return <h1>Homepage</h1>;
+  return (
+    <>
+      <Header />
+
+      {/* TODO: <Category /> */}
+      <section className="category">
+        <h2 className="category__tilte">Fruits & Légumes</h2>
+        {/* TODO: <Products /> */}
+        <div className="product__container">
+          <Product />
+        </div>
+      </section>
+
+      <Footer />
+    </>
+  );
 }
